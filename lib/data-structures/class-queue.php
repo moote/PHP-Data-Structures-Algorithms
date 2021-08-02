be799c186ec5bacfe63f2b2fa285f6747e28489b
Rich Conaway <rich@idunnit.com>
2021-08-02 14:20:02 -0700

    Initial queue commit
    Testing PRs initial queue commit

---
+++ lib/data-structures/class-queue.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PDSA;
+
+use Exception;
+use InvalidArgumentException;
+
+/**
+ * Array wrapper class the implements simple queue functions
+ * - the queue can only accept char or int data types
+ * - the queue is an immutable fixed size
+ * 
+ */
+class Queue {
+
+}
